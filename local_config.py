dece9be3fa08834e1d9c9f3e37a987af8ebfea9b
Michael Kali <michael.kali@weizmann.ac.il>
2024-10-22 15:27:41 +0300

    add teledyne_camera_viewer.ipynb and remove loca_config from git

--- local_config.py
+++
@@ -1,6 +0,0 @@
-# In this file, you can define the path to your data folder, and other computer dependant values.
-# This way, you can avoid hardcoding the path to your data folder in your scripts.
-
-DATA_PATH = r"C:\Users\michaeka/Dropbox (Weizmann Institute)\Lab's Dropbox"
-path_data_local = r'C:\data_bank'
-path_data_dropbox = None
