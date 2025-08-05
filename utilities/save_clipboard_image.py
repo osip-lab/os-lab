bcf309131231d13a761685363ca5d64931ea521c
OSIPLAB <osip.lab@weizmann.ac.il>
2025-08-05 10:53:49 +0300

    more general save_clipboard_image.py

--- utilities/save_clipboard_image.py
+++ utilities/save_clipboard_image.py
@@ -1,9 +1,23 @@
 from PIL import ImageGrab
 from datetime import datetime
+import os
 
+# Get current timestamp
 now = datetime.now()
-
 current_time = now.strftime("%y%m%d%H%M%S")
 
-im=ImageGrab.grabclipboard()
-im.save(r"C:\Users\michaeka\Desktop\{}.png".format(current_time))
+# Grab image from clipboard
+im = ImageGrab.grabclipboard()
+
+# Get the user's desktop path
+desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
+
+# Construct full path for saving the image
+save_path = os.path.join(desktop_path, f"{current_time}.png")
+
+# Save the image
+if im:
+    im.save(save_path)
+    print(f"Image saved to: {save_path}")
+else:
+    print("No image found in clipboard.")
