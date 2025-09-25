3d4fa1d97ef91208f674bdbeab9447b4a50ef1ce
AlexVasilyev-WIS <alexandr.vasilyev@weizmann.ac.il>
2025-09-25 14:57:50 +0300

    Added in measure_line_shape.py switch to turn off the fitting

--- rigol_dsa/measure_line_shape.py
+++ rigol_dsa/measure_line_shape.py
@@ -8,7 +8,7 @@ from scipy.optimize import curve_fit
 from PyQt6.QtWidgets import QApplication, QWidget, QCheckBox, QAbstractSpinBox
 from PyQt6.QtCore import pyqtSignal, pyqtSlot, QTimer
 
-from qt_gui.qt_ext import MyStandardWindow, QMyVBoxLayout, QMySpinBox, ThreadedWidget
+from qt_gui.qt_ext import MyStandardWindow, QMyVBoxLayout, QMyHBoxLayout, QMySpinBox, ThreadedWidget
 from rigol_dsa import RigolDSAWidget
 
 from local_config import PATH_DATA_LOCAL
@@ -39,9 +39,13 @@ class FitterWidget(ThreadedWidget):
         super(FitterWidget, self).__init__(font_size=font_size)
         self.setTitle('Line Shape Fitter')
 
-        self.switch = QCheckBox('continuous')
-        self.switch.setChecked(False)
-        self.switch.setToolTip('enable continuous measurement')
+        self.switch_fit = QCheckBox('fit')
+        self.switch_fit.setChecked(False)
+        self.switch_fit.setToolTip('enable fitting')
+
+        self.switch_auto = QCheckBox('continuous')
+        self.switch_auto.setChecked(False)
+        self.switch_auto.setToolTip('enable continuous measurement')
 
         self.labels = ('FSR', 'TpL', 'ampl', 'f_off')
         self.spinboxes = dict()
@@ -59,11 +63,18 @@ class FitterWidget(ThreadedWidget):
         self.on_wait = None
         self.moment.timeout.connect(self.timeout)
 
-        layout = QMyVBoxLayout(self.switch, *(self.spinboxes[lbl] for lbl in self.labels))
+        layout = QMyVBoxLayout()
+        layout.addLayout(QMyHBoxLayout(self.switch_fit, self.switch_auto))
+        for lbl in self.labels:
+            layout.addWidget(self.spinboxes[lbl])
         self.setLayout(layout)
 
-    def wait_and_emit(self, signal):
+    def wait_and_emit(self, signal, delay=None):
         self.on_wait = signal
+        if delay is None:
+            self.moment.setInterval(10)
+        else:
+            self.moment.setInterval(int(delay))
         self.moment.start()
 
     def timeout(self):
@@ -77,22 +88,28 @@ class FitterWidget(ThreadedWidget):
         # data = data.transpose()
         # data = {'freq': data[0], 'ampl': data[1]}
 
-        try:
-            data = fit_line_shape(data['freq'], data['ampl'])
+        if self.switch_fit.isChecked():
+            try:
+                data = fit_line_shape(data['freq'], data['ampl'])
 
-            self.spinboxes['FSR'].setValue(data['fsr'])
-            self.spinboxes['TpL'].setValue(data['tpl'] * 1e6)
-            self.spinboxes['ampl'].setValue(data['ampl'] * 1e3)
-            self.spinboxes['f_off'].setValue(data['freq_offset'])
+                self.spinboxes['FSR'].setValue(data['fsr'])
+                self.spinboxes['TpL'].setValue(data['tpl'] * 1e6)
+                self.spinboxes['ampl'].setValue(data['ampl'] * 1e3)
+                self.spinboxes['f_off'].setValue(data['freq_offset'])
 
-            log_msg = (f"measured line shape - FSR = {data['fsr']:013.3f} Hz, T + L = {data['tpl'] * 1e6:07.3f} ppm, "
-                       f"ampl = {data['ampl'] * 1e3:010.6f}, frequency offset = {data['freq_offset']:07.3f} Hz")
-            logging.info(log_msg)
-        except RuntimeError:
-            logging.info('line shape measurement failed')
+                log_msg = (f"measured line shape - FSR = {data['fsr']:013.3f} Hz, T + L = {data['tpl'] * 1e6:07.3f} ppm, "
+                           f"ampl = {data['ampl'] * 1e3:010.6f}, frequency offset = {data['freq_offset']:07.3f} Hz")
+                logging.info(log_msg)
+            except RuntimeError:
+                logging.info('line shape measurement failed')
+        else:
+            logging.info('line shape measurement skipped')
 
-        if self.switch.isChecked():
-            self.wait_and_emit(self.sig_start)
+        if self.switch_auto.isChecked():
+            if self.switch_fit.isChecked():
+                self.wait_and_emit(self.sig_start)
+            else:
+                self.wait_and_emit(self.sig_start, delay=300)
 
 
 class MainWidget(QWidget):
